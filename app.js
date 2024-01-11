0b55f2d42396b6872aef804614adaf1c3f5790ff
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-01-11 15:57:29 +0100

    Add view engine and routes

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const path = require('path');
 
 const dbConnection = require('./lib/db');
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    dbConnection.query('SELECT * FROM Kunder', (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('kunder', { data:rows });
+        }
+    });
+});
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "public/404.html"));
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
